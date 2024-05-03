6a3cefccebc7f7bc0b3d057a14ab92096e30ff15
SapnaAcharya <acharyasapna2059@gmail.com>
2024-05-03 11:20:16 +0545

    update javascript

---
+++ first.js
@@ -0,0 +1,43 @@
+console.log("Welcome to Apna College");
+console.log("Sapna Acharya");
+console.log("I love javascript");
+
+// defining variables
+console.log("Tony Stack")
+
+fullname = "Tony Stack";
+console.log(fullname);
+age = 24;   //24 is assigned to variable age
+price = 99.99;
+x = null;
+y = undefined;
+console.log(x);
+isfollow = false;  //boolean value store
+console.log(isfollow);
+
+fname = "sapna acharya";
+FNAME = "aman gupta";
+console.log(fname);
+console.log(FNAME);
+
+let fullname = "tony stark";
+console.log(fullname);
+
+let age =24;
+age = 25;
+console.log(age);
+
+const address = 24;
+console.log(address);
+
+{
+    let a = 5;
+    console.log(a);
+}
+
+{
+    let a = 10;
+    console.log(a);
+}
+
+
